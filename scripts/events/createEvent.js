a2b1eb76138aba28de1875a99a000de4157583fc
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-09 18:37:31 +0800

    test

--- scripts/events/createEvent.js
+++ scripts/events/createEvent.js
@@ -3,18 +3,48 @@ import { renderEvents } from './events.js';
 import { getDateTime } from '../common/time.utils.js';
 import { closeModal } from '../common/modal.js';
 
-const eventFormElem = document.querySelector('.event-form');
 const closeEventFormBtn = document.querySelector('.create-event__close-btn');
+const submitBtn = document.querySelector('.event-form__submit-btn')
+
+const titleInput = document.querySelector(`input[name='title']`);
+const dateInput = document.querySelector(`input[name='date']`);
+const startTimeInput = document.querySelector(`input[name='startTime']`);
+const endTimeInput = document.querySelector(`input[name='endTime']`);
+const descriptionInput = document.querySelector(`textarea[name='description']`);
+
+const events = [];
 
 function clearEventForm() {
+  titleInput.value = '';
+  dateInput.value = '';
+  startTimeInput.value = '';
+  endTimeInput.value = '';
+  descriptionInput.value = '';
   // ф-ция должна очистить поля формы от значений
 }
 
-function onCloseEventForm() {
-  // здесь нужно закрыть модальное окно и очистить форму
+function onCloseEventForm(event) {
+  closeModal()
 }
 
 function onCreateEvent(event) {
+  event.preventDefault();
+  
+  const newEvent = {
+    id: Math.random(), // id понадобится для работы с событиями
+    title: `${titleInput.value}`,
+    description: `${descriptionInput.value}`,
+    start: new Date(`${dateInput.value}` + ` ${startTimeInput.value}`),
+    end: new Date(`${dateInput.value}` + ` ${endTimeInput.value}`),
+  };
+  
+  if (titleInput.value) {
+    events.push(newEvent)
+    setItem('events', events);
+  }
+  clearEventForm()
+  onCloseEventForm();
+  console.log(newEvent);
   // задача этой ф-ции только добавить новое событие в массив событий, что хранится в storage
   // создавать или менять DOM элементы здесь не нужно. Этим займутся другие ф-ции
   // при подтверждении формы нужно считать данные с формы
@@ -27,5 +57,7 @@ function onCreateEvent(event) {
 }
 
 export function initEventForm() {
+  closeEventFormBtn.addEventListener('click', onCloseEventForm);
+  submitBtn.addEventListener('click', onCreateEvent)
   // подпишитесь на сабмит формы и на закрытие формы
 }
